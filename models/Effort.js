576a7c854e8696646533ee3d5d0def389b6f781e
Kevin Ott <kevin.ott93@gmail.com>
2015-10-14 11:01:25 +0200

    new date field added to effort

--- models/Effort.js
+++ models/Effort.js
@@ -5,6 +5,7 @@ var effortSchema = mongoose.Schema(
 	amount: {type: Number, default: null, required: true},
 	module: { type: String, ref: 'Module', required: true },
 	studentid: { type: String, ref: 'Student', required: true},
+	date: {tyoe: Date, required: true, default: Date.now()},
     category: {type: String}
 },
 { 
