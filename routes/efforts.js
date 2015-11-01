2a1021447624d91cc33bc56190ae860232a7f686
seiker <wolfthorina@hotmail.com>
2015-11-01 22:57:03 +0100

    Update efforts.js
    untested yet

--- routes/efforts.js
+++ routes/efforts.js
@@ -5,8 +5,8 @@
 var effortdb = require('../models/Effort.js');
 var studdb = require('../models/Student.js');
 var identdb = require('../models/Identification.js');
-var moduledb = require('../models/Module');
-var efforttypedb = require('../models/EffType');
+var moduledb = require('../models/Module.js');
+var efforttypedb = require('../models/EffType.js');
 var async = require('async');
 
 exports.getEffortsByStudent = function (req, res) {
@@ -53,7 +53,52 @@ exports.getEffortById = function (req, res) {
 };
 //FIXME: just broken...
 exports.getEffortsByModule = function (req, res) {
-    // Student can view a list of his efforts when selecting a module
+    // Student can view a list of his efforts for one selected module
+    // Result contains all information about the efforts, however, not all must be used
+    var studentId = req.params.studentid;
+	var moduleId = req.params.moduleid;
+    async.series([
+        function(callback) {
+            console.log("Checking for student: " + studentId);
+            studdb.studentModel.count({ _id: studentId }, function(err, count) {
+                //console.log("Found: " + count);
+                if(count > 0) {
+                    console.log("Student found. Searching database for module.");
+                    callback()
+                } else {
+                    callback("Student not in Database!");
+                }
+            });
+        },
+		function(callback) {
+            console.log("Checking for module: " + moduleId);
+            moduledb.moduleModel.count({ _id: moduleId }, function(err, count) {
+                //console.log("Found: " + count);
+                if(count > 0) {
+                    console.log("Module found. Searching database for efforts.");
+                    callback()
+                } else {
+                    callback("Module not in Database!");
+                }
+            });
+        },
+        function(callback) {
+            effortdb.effortModel.find( { createdBy: studentId, module: moduleId }, function(err, result) {
+                if(err) console.log(err);
+                else callback(result);
+            });
+        }
+    ], function (err, result) {
+        console.log("Done. Sending results.");
+        if(err) res.status(500).send(err);
+        else if(result) res.status(200).send(result);
+    });
+};
+  
+  
+  /* 
+
+   // Student can view a list of his efforts when selecting a module
     // Result contains all information about the efforts, however, not all must be used
     var session = req.headers['jsessionid'];
     // req.sessionID ??
@@ -96,6 +141,9 @@ exports.getEffortsByModule = function (req, res) {
         });
     // get the information about efforts through population, select only where module is the given moduleid
 };
+*/
+
+
 
 //FIXME: Broken as fuck
 exports.getEffortsByType = function (req, res) {
@@ -157,11 +205,12 @@ exports.createEffort = function(req, res) {
     //console.log(req.body);
     var modId = req.body.moduleid;
     var studId = req.body.studentid;
+	var efftypeId = req.body.efforttypeid;
 
     async.parallel([
         function(callback) {
 
-            console.log(modId + " " + studId);
+            console.log(modId + " " + studId + " " + efftypeId);
             moduledb.moduleModel.findById(modId, function(err, result) {
                 if (err) {
                     callback(err);
@@ -188,14 +237,38 @@ exports.createEffort = function(req, res) {
                 }
             });
 
+        },
+		function(callback) {
+            efforttypedb.effTypeModel.findById(efftypeId, function(err, result) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                if (result == undefined) {
+                    callback("Effort Type not found");
+                    return;
+                } else {
+                    callback(null, result);
+                }
+            });
+
         }
     ], function(err, results) {
-        if(results.length == 2) {
-            //result[0] = Module, result[1] = Student
+        if(results.length == 3) {
+            //result[0] = Module, result[1] = Student, result[2] = EffortType
             var newEffort = new effortdb.effortModel();
             newEffort.amount = req.body.amount;
             newEffort.module = results[0]["_id"];
             newEffort.createdBy = results[1]["_id"];
+			newEffort.type = results[2]["_id"];
+			newEffort.performanceDate = new Date(req.body.performanceDate); 
+			// "<YYYY-mm-dd>" Format
+			if(req.body.material !== undefined) {
+                newEffort.material = req.body.material;
+            }
+            if(req.body.place !== undefined) {
+                newEffort.place = req.body.place;
+            } 
             newEffort.save(function(err, result) {
                 if(err) res.status(500).send("Failed to create effort");
                 else if(result) {
@@ -208,7 +281,7 @@ exports.createEffort = function(req, res) {
             //console.log(newEffort);
             //res.status(200).send(newEffort);
         } else {
-            res.status(404).send("User or module not in database");
+            res.status(404).send("User, module or effort type not in database");
         }
     });
     return;
@@ -236,6 +309,83 @@ exports.createEffort = function(req, res) {
 
 //FIXME: Broken as Fuck
 exports.updateEffort = function(req, res) {
+    //console.log(req.body);
+    var modId = req.body.moduleid;
+    var effId = req.body.effortid;
+	var efftypeId = req.body.efforttypeid;
+
+    async.parallel([
+        function(callback) {
+
+            console.log(modId + " " + effId + " " + efftypeId);
+            moduledb.moduleModel.findById(modId, function(err, result) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                if (result == undefined) {
+                    callback("Module not found");
+                } else {
+                    callback(null, result);
+                }
+            });
+        },
+		function(callback) {
+            efforttypedb.effTypeModel.findById(efftypeId, function(err, result) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                if (result == undefined) {
+                    callback("Effort Type not found");
+                    return;
+                } else {
+                    callback(null, result);
+                }
+            });
+
+        }
+    ], function(err, results) {
+        if(results.length == 2) {
+            //result[0] = Module, result[1] = EffortType
+			
+			effortdb.effortModel.findById(effId, function(err, eff) {
+            if (err){
+                res.send(err);
+                return;
+			}
+			else {
+            eff.module = results[0]["_id"];
+            eff.type = results[1]["_id"];
+			eff.amount = req.body.amount;
+			eff.performanceDate = new Date(req.body.performanceDate); 
+			// "<YYYY-mm-dd>" Format
+			if(req.body.material !== undefined) {
+                eff.material = req.body.material;
+            }
+            if(req.body.place !== undefined) {
+                eff.place = req.body.place;
+            } 
+            // save the bear
+            eff.save(function(err, result) {
+                if(err) res.status(500).send("Failed to update effort");
+                else if(result) {
+                    var message = {};
+                    message.success = true;
+                    message.id = result._id;
+                    res.status(200).send(message);
+                }
+            })
+			}
+			}
+            //console.log(eff);
+            //res.status(200).send(eff);
+        } else {
+            res.status(404).send("Module or effort not in database");
+        }
+    });
+    return;	
+	
     var newamount = req.body.amount;
     var newmodule = moduledb.moduleModel.findOne({name: req.body.module});
     var newtype = efforttypedb.effTypeModel.findOne({name: req.body.type});
