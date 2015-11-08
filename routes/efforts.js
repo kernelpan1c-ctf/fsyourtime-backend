d7d1181a5775f84821d868bde8719fd692dcf780
seiker <wolfthorina@hotmail.com>
2015-11-08 17:21:18 +0100

    Update efforts.js
    deleteeffort added
    tested on AWS.

--- routes/efforts.js
+++ routes/efforts.js
@@ -96,110 +96,6 @@ exports.getEffortsByModule = function (req, res) {
 };
   
   
-  /* 
-
-   // Student can view a list of his efforts when selecting a module
-    // Result contains all information about the efforts, however, not all must be used
-    var session = req.headers['jsessionid'];
-    // req.sessionID ??
-    var matricularnr;
-    identdb.findOne({jsession: session}, function (err, identification) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            matricularnr = identification.relmatricularnr;
-        }
-    });
-    // find out which student is logged in
-    var effortlist = [];
-    studdb.studentModel.find({_id: matricularnr}, function (err, student) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            effortlist = student.efforts;
-        }
-    });
-    // get all the efforts a student has registered
-    effortdb.effortModel
-        .find({_id: {$in: effortlist}, module: req.params.moduleid})
-        .populate({
-            path: 'module'
-        },
-        {
-            path: 'type',
-            select: 'name relcategory'
-        })
-        .exec(function (err, efforts) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                res.json(efforts);
-            }
-        });
-    // get the information about efforts through population, select only where module is the given moduleid
-};
-*/
-
-
-
-//FIXME: Broken as fuck
-exports.getEffortsByType = function (req, res) {
-    // Student can view a list of his efforts when selecting a type
-    // Result contains all information about the efforts, however, not all must be used
-    var session = req.headers['jsessionid'];
-    // req.sessionID ??
-    var matricularnr;
-    identdb.findOne({jsession: session}, function (err, identification) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            matricularnr = identification.relmatricularnr;
-        }
-    });
-    // find out which student is logged in
-    var effortlist = [];
-    studdb.studentModel.find({_id: matricularnr}, function (err, student) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            effortlist = student.efforts;
-        }
-    });
-    // get all the efforts a student has registered
-    effortdb.effortModel
-        .find({_id: {$in: effortlist}, type: req.params.efftypeid})
-        .populate({
-            path: 'module'
-        },
-        {
-            path: 'type',
-            select: 'name relcategory'
-        })
-        .exec(function (err, efforts) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                res.json(efforts);
-            }
-        });
-    // get the information about efforts through population, select only when type is the given typeid
-};
-/*
- exports.createEffort = function(req, res) {
- var amount = req.body.amount;
- var module = req.body.module;
- var student = req.body.studentid;
-
- res.send("Created new effort: \n\tAmount: " + amount + "\n\tModule: " + module + "\n\tStudent: " + student);
-
- }
- */
 
 exports.createEffort = function(req, res) {
     //console.log(req.body);
@@ -287,29 +183,10 @@ exports.createEffort = function(req, res) {
         }
     });
     return;
-    var newEffort = new effortdb.effortModel();
-    newEffort.amount = req.body.amount;
-    newEffort.module = moduledb.moduleModel.findOne({name: req.body.module}).exec();
-    //newEffort.type = efforttypedb.effTypeModel.findOne({name: req.body.type}).exec();
-    newEffort.matricularnr = studdb.studentModel.findById(req.body.matricularnr).exec();
-    newEffort.performanceDate = new Date(req.body.performanceDate); // "<YYYY-mm-dd>" Format
-    newEffort.material = req.body.material;
-    newEffort.place = req.body.place;
-
-    console.log(JSON.stringify(newEffort), null, 3);
-
-    newEffort.save(function(err, result) {
-        if (err) {
-            console.log(err);
-            return res.sendStatus(500);
-        }
-        else {
-            return res.send(newEffort);
-        }
-    });
+    
 };
 
-//FIXME: Broken as Fuck
+
 exports.updateEffort = function(req, res) {
     //console.log(req.body);
     var modId = req.body.moduleid;
@@ -353,7 +230,8 @@ exports.updateEffort = function(req, res) {
             //result[0] = Module, result[1] = EffortType
 			
 			effortdb.effortModel.findById(effId, function(err, eff) {
-            if (err){
+  
+			if (err){
                 res.send(err);
                 return;
 			}
@@ -393,40 +271,24 @@ exports.updateEffort = function(req, res) {
     });
     return;	
 	
-    var newamount = req.body.amount;
-    var newmodule = moduledb.moduleModel.findOne({name: req.body.module});
-    var newtype = efforttypedb.effTypeModel.findOne({name: req.body.type});
-    var newperformanceDate = new Date(req.body.performanceDate); // "<YYYY-mm-dd>" Format
-    var newcreationDate = new Date();
-    var newmaterial = req.body.material;
-    var newplace = req.body.place;
-
-    efforttypedb.findByIdAndUpdate(req.params.effortid, {amount: newamount, module: newmodule, type: newtype, performanceDate: newperformanceDate, creationDate: newcreationDate, material: newmaterial, place: newplace},function (err, effort) {
-        if (err) {
-            console.log(err);
-            return res.sendStatus(500);
-        }
-        else {
-            return res.send('We did it!');
-        }
-    });
+   
 };
 
-//FIXME: Broken as Fuck
+
 exports.deleteEffort = function(req, res) {
-    effortdb.findByIdAndRemove(req.params.effortid, function (err) {
-        if (err) {
-            console.log(err);
-            return res.sendStatus(500);
-        }
-        else { return res.sendStatus(200)}
-    });
-
-
+	var effId = req.params.effortid;
+	var studId = req.body.studentid;
+	console.log(effId + "  " + studId);
+            // delete the effort
+            effortdb.effortModel.remove({_id: effId, createdBy: studId}, function(err, eff) {
+            if (err)
+                res.send(err);
+            res.status(200).send("Successfully deleted!");;
+			});	
 };
 
-//FIXME: Broken as Fuck
-exports.deleteAllMyEfforts = function(req, res) {
+
+/*exports.deleteAllMyEfforts = function(req, res) {
     effortdb.remove({matricularnr: req.params.matricularnr}, function (err) {
         if (err) {
             console.log(err);
@@ -438,3 +300,4 @@ exports.deleteAllMyEfforts = function(req, res) {
 
 };
 //TODO: Validate Effort (update/enter) --> Date of effort not more than 2 weeks in past!
+*/
