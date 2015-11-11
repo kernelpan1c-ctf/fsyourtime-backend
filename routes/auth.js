d2539d2cb74e7f4307a1f988c7e8b079e3dbe64f
Kevin Ott <kevin.ott93@gmail.com>
2015-11-11 18:23:22 +0100

    bugfix

--- routes/auth.js
+++ routes/auth.js
@@ -69,6 +69,7 @@ exports.login = function (req, res) {
             return callback(null, userinfo);
         },
         function(userInfo, callback) {
+            console.log(userInfo);
             if(!userInfo.sync) {
                 console.log("Skipping module request");
                 return callback(null, userInfo, null);
