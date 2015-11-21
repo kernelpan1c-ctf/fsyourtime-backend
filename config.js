4adf7bc39f6511a0b43863621890f8060192b879
Kevin Ott <kevin.ott93@gmail.com>
2015-11-21 14:42:49 +0100

    disable TLS

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     dbUrl: 'mongodb://localhost/fsyourtime',
-    useHttps: true,
+    useHttps: false,
     loginUrl: function(username, password) { return 'https://campus.frankfurt-school.de/clicnetclm/loginService.do?xaction=login&username=' + username + '&password=' + encodeURIComponent(password) },
     studentInfoUrl: 'https://campus.frankfurt-school.de/clicnetclm/campusAppStudentX.do?xaction=getStudentData',
     sslKeyLocation: '../certs/server.key',
